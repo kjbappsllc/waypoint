6e46d487cd1ea01bbc1cd6e93a103b04caaa3a37
Keyshawn Butts <kjbappsllc@gmail.com>
2021-09-21 17:12:25 -0400

    updated the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
-
 module.exports = {
   prefix: 'tw-',
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['./src/**/*.html', './src/**/*.js'],
+  },
   darkMode: false,
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    },
     extend: {},
   },
   variants: {},
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography')
-  ]
-}
-
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio'), require('@tailwindcss/typography')],
+};
