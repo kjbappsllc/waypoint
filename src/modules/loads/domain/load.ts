7ea7023af9cd161f04487dec45117b99ff7942a2
Keyshawn Butts <kjbappsllc@gmail.com>
2021-09-21 21:21:52 -0400

    Added more to the loads module

--- src/modules/loads/domain/load.ts
+++ src/modules/loads/domain/load.ts
@@ -1,6 +1,17 @@
 import { Currency } from "../../shared/core/currency";
+import { EquipmentType } from "./equipment-type";
+import { Location } from "./location";
 
 export interface Load {
+  creatorId: string;
   timeCreated: Date;
   rate: Currency;
+  origin: Location;
+  stops: Location [];
+  destination: Location;
+  shipDate: Date;
+  deliveryDate: Date;
+  weight: number;
+  equipmentType: EquipmentType;
+  miles: number;
 }
